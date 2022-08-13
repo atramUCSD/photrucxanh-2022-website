85cb6a542923eac29d800b22173f3006338293bd
atramUCSD <atram@ucsd.edu>
2022-08-12 22:11:35 -0700

    8/12/2022 Update

---
+++ food-yummy/src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect } from "react";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Newsletter from "./components/Newsletter";
+import Portfolio from "./components/Portfolio";
+import Products from "./components/Products";
+import ScrollToTop from "./components/ScrollToTop";
+import Services from "./components/Services";
+import Testimonials from "./components/Testimonials";
+import scrollreveal from "scrollreveal";
+import { SliderData } from './components/SliderData';
+import ImageSlider from "./components/ImageSlider";
+
+export default function App() {
+  useEffect(() => {
+    const sr = scrollreveal({
+      origin: "top",
+      distance: "80px",
+      duration: 2000,
+      reset: false,
+    });
+    sr.reveal(
+      `
+        nav,
+        #home,
+        #services,
+        #portfolio,
+        #testimonials,
+        #products,
+        #newsletter,
+        .footer
+    `,
+      {
+        opacity: 0,
+        interval: 200,
+      }
+    );
+  }, []);
+  return (
+    <>
+      <ScrollToTop />
+      <Navbar />
+      <ImageSlider slides={SliderData}/>
+      <Hero />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Products />
+      <Newsletter />
+      <Footer />
+    </>
+  );
+}
